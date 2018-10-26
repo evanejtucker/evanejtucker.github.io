7b86b61328210fe8d0d429963cd7fa69a8c913ef
Evan Tucker <eej.tucker@gmail.com>
2018-10-26 12:01:09 -0600

    fresh start :ghost:

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    // materialize declarations
+    // =============================================================================================
+    $('.sidenav').sidenav();
+
+
+
+    // global variables
+    // =============================================================================================
+
+
+
+
+    // functions
+    // =============================================================================================
+
+    function navScrollEffect() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            $(".main-nav").addClass('fade-height')
+        } else {
+            $(".main-nav").removeClass('fade-height')
+        }
+    }
+
+
+    // main process
+    // =============================================================================================
+    
+    window.onscroll = function() {navScrollEffect()};
+
+
+});
