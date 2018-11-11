382b78936ff3b43c84eb07f1e48d81dea0976126
Evan Tucker <eej.tucker@gmail.com>
2018-11-10 18:46:23 -0700

    added animations to about section :game_die:

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
 
-    // materialize declarations
-    // =============================================================================================
+// materialize declarations
+// =============================================================================================
     $('.sidenav').sidenav();
 
 
 
-    // global variables
-    // =============================================================================================
+// global variables
+// =============================================================================================|
 
 
 
 
-    // functions
-    // =============================================================================================
+// functions
+// =============================================================================================|
 
+    // scroll effect when user scrolls off of the nav
     function navScrollEffect() {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
             $(".main-nav").addClass('fade-height');
@@ -23,12 +24,32 @@ $(document).ready(function(){
         }
     }
 
-    // main process
-    // =============================================================================================
-    
-    window.onscroll = function() {navScrollEffect()};
+    // scroll effect that happens over the about section
+    function aboutScrollEffect() {
+        var hT = $('#about').offset().top,
+            hH = $('#about').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT + hH - wH)) {
+            $(".profilePic").addClass("animated slideInLeft");
+            $(".bioText").addClass("animated slideInRight");
+            $(".profilePic").show();
+            $(".bioText").show();
 
-    // $(".nav-menu li a").on('click', scrollTo);
+        }
+    }
+
+// main process
+// =============================================================================================|
+    
+    $(".profilePic").hide();
+    $(".bioText").hide();
+
+    window.onscroll = function() {
+        navScrollEffect();
+        aboutScrollEffect();
+    };
+
 
 
 });
