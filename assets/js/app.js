7e1a3ddde5336b2ec6036a19c48ecbca34bf3ae7
Evan Tucker <eej.tucker@gmail.com>
2018-11-12 11:01:41 -0700

    automated skills section :bowling:

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
+
+
 $(document).ready(function(){
 
 // materialize declarations
 // =============================================================================================
     $('.sidenav').sidenav();
 
-
+    var firstname = 'evan';
 
 // global variables
 // =============================================================================================|
@@ -39,15 +41,71 @@ $(document).ready(function(){
         }
     }
 
+    // scroll effect that happens over the skills section
+    function skillsScrollEffect() {
+        var hT = $('#skills').offset().top,
+            hH = $('#skills').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT + hH - wH)) {
+            $(".skillsList").addClass("animated zoomIn");
+            $(".skillsList").show();
+        }
+    }
+
+    // dynamically add skills to page
+    function addSkills() {
+        for (var i=0; i<skills.length; i++) {
+            var div = $("<div>");
+            div.addClass("skillsList");
+
+            var img = $("<img>");
+            img.addClass("skillsImage");
+            img.attr("src", skills[i].image);
+            img.attr("alt", "skills-icon");
+
+            var p = $("<p>");
+            p.addClass("skillsTitle");
+            p.text(skills[i].title);
+
+            var ul = $("<ul>");
+            if (!skills[i].image  && !skills[i].title) {
+                console.log('no image or title');
+            } else if (!skills[i].image && skills[i].title) {
+                ul.append(p);
+            } else if (skills[i].image && !skills[i].title) {
+                ul.append(img);
+            } else {
+                ul.append(img, p);
+            }
+
+            for (var j=0; j<skills[i].skills.length; j++) {
+                var li = $("<li>");
+                li.text(skills[i].skills[j]);
+                ul.append(li);
+            }
+
+            div.append(ul);
+            $("#skills").append(div);
+            
+        }
+    }
+
 // main process
 // =============================================================================================|
-    
+
+    console.log("loaded second");
+    console.log(skills);
+    addSkills();
+
     $(".profilePic").hide();
     $(".bioText").hide();
+    $(".skillsList").hide();
 
     window.onscroll = function() {
         navScrollEffect();
         aboutScrollEffect();
+        skillsScrollEffect();
     };
 
 
---
+++ assets/js/variables.js
@@ -0,0 +1,24 @@
+
+// skills lists that will be appended to the skills section in the html 
+var skills = [
+    // skill list 1
+    {
+        title: "Front End Skills",
+        image: "assets/images/front-end.png",
+        skills: ["HTML5", "CSS3", "SASS", "JavaScript", "jQuery", "React"]
+    },
+    // skill list 2
+    {
+        title: "Back End Skills",
+        image: "assets/images/gears.svg",
+        skills: ["Node", "Express", "MongoDB", "MySQL", "Template Engines", "MERN"]
+    },
+    // skill list 3
+    {
+        title: "Other Skills",
+        image: "assets/images/otherSkills.png",
+        skills: ["Customer Service", "Microsoft Office", "Public Speaking", "Marketing"]
+    }
+]
+
+
